67569433f225b90d85b08182746f26ddbd9ff29f
Dan Leehr <dan.leehr@nescent.org>
2013-12-03 15:38:45 -0500

    Working demo of dryad author editor
    Just a prototype.  Yay hacking.

--- app/js/app.js
+++ app/js/app.js
@@ -6,11 +6,10 @@ angular.module('myApp', [
   'ngRoute',
   'myApp.filters',
   'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+//   'myApp.directives',
+//   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.when('/author_editor', {templateUrl: 'partials/author_editor.html', controller: 'AuthorEditorCtrl'});
+  $routeProvider.otherwise({redirectTo: '/author_editor'});
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,30 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
-  }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
+function AuthorEditorCtrl ($scope) {
+    $scope.authors = [];
+    $scope.addAuthor = function(author) {
+      $scope.authors.push(angular.copy(author));
+    };
+    $scope.removeAuthor = function(index) {
+      $scope.authors.splice(index,1);
+    };
+    
+    $scope.move = function (array, old_index, new_index) {
+      if (new_index >= array.length) {
+          var k = new_index - array.length;
+          while ((k--) + 1) {
+              array.push(undefined);
+          }
+      }
+      array.splice(new_index, 0, array.splice(old_index, 1)[0]);
+      return array; // for testing purposes
+    };
+    $scope.moveUp = function(index) {
+      $scope.move($scope.authors, index, index - 1);
+    };
+    
+    $scope.moveDown = function(index) {
+      $scope.move($scope.authors, index, index + 1);
+    };
+}
